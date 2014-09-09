4edbf2f74b3cee8375bc3322af446470a6f40066
mokumoku <da1_1hara@me.com>
2014-09-09 15:03:13 +0900

    i524のレビュー指摘修正

--- donmaru.rb
+++ donmaru.rb
@@ -54,36 +54,33 @@ bot.on :message do |m|
 end
 
 # 税込価格を計算
-def calcDonPrice(税抜価格)
+def 税抜き(税抜価格)
     消費税 = 1.08
     価格 = (税抜価格 * 消費税).round
+    # 1の位を 0-4 => 0, 5-9 => 5 にする
     一の位 = 価格 % 10
-    if 一の位 < 5
-        価格 -= 一の位
-    else
-        価格 -= (一の位 - 5)
-    end
+    価格 -= 一の位 % 5
     return 価格
 end
     
 #並盛り
 def putsRegularSizePrice(m)
-    m.reply "並盛りは#{calcDonPrice(500)}円（税込）だどん"
+    m.reply "並盛りは#{税抜き(500)}円（税込）だどん"
 end
 
 #大盛り
 def putsLargeSizePrice(m)
-    m.reply "ご飯大盛りは#{calcDonPrice(600)}円（税込）だどん"
+    m.reply "ご飯大盛りは#{税抜き(600)}円（税込）だどん"
 end
 
 #ネタ大盛り
 def putsSushiMaterialLargeSizePrice(m)
-    m.reply "ネタ大盛りは#{calcDonPrice(700)}円（税込）だどん"
+    m.reply "ネタ大盛りは#{税抜き(700)}円（税込）だどん"
 end
 
 #特盛り
 def putsSpecialSizePrice(m)
-    m.reply "特盛りは#{calcDonPrice(800)}円（税込）だどん"
+    m.reply "特盛りは#{税抜き(800)}円（税込）だどん"
 end
 
 #競合店
